b7f202387cc1e54964820de9a0fa17bb1664536a
JF Nepton <sqasolution@hotmail.com>
2017-01-19 18:28:38 -0800

    Create requestIntFromUser Methods

--- src/main/java/com/sqa/jf/helpers/AppBasics.java
+++ src/main/java/com/sqa/jf/helpers/AppBasics.java
@@ -9,6 +9,8 @@ package com.sqa.jf.helpers;
 
 import java.util.*;
 
+import com.sqa.jf.helpers.exceptions.*;
+
 /**
  * AppBasics //ADDD (description of class)
  * <p>
@@ -35,10 +37,33 @@ public class AppBasics {
 		return scanner.nextLine();
 	}
 
-	public static String requestInfoFromUser(String question) {
-		String response;
-		System.out.print(question + " ");
-		response = scanner.nextLine();
-		return response;
+	public static int requestIntFromUser(String question) {
+		return requestIntFromUser(question, 0, 0);
+	}
+
+	public static int requestIntFromUser(String question, int min, int max) {
+		String input;
+		int result = 0;
+		boolean isNotValid = true;
+		while (isNotValid) {
+			try {
+				System.out.print(question + " ");
+				input = scanner.nextLine();
+				result = Integer.parseInt(input);
+				if (min != 0 && max != 0) {
+					if (result <= max && result >= min) {
+						isNotValid = false;
+					} else {
+						throw new NumberNotInRangeException();
+					}
+				}
+				isNotValid = false;
+			} catch (NumberFormatException e) {
+				System.out.println("You have not entered a correct number value.");
+			} catch (NumberNotInRangeException e) {
+				System.out.println("You have not entered a number within the range of " + min + " and " + max + ".");
+			}
+		}
+		return result;
 	}
 }
---
+++ src/main/java/com/sqa/jf/helpers/exceptions/NumberNotInRangeException.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: NumberNotInRangeException.java<br>
+ *
+ *   Nepton, Jean-francois<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor<br>
+ *   Created: Jan 18, 2017
+ *   
+ */
+
+package com.sqa.jf.helpers.exceptions;
+
+
+/**
+ * NumberNotInRangeException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Nepton, Jean-francois
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class NumberNotInRangeException extends Exception {
+}
