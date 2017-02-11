954f4d88233872c5bbaa92d5e3721fe2a5eb991e
JF Nepton <sqasolution@hotmail.com>
2017-02-11 13:41:21 -0800

    Implement DataHelper getTextFileData method

---
+++ src/main/java/com/sqa/jf/helpers/DataType.java
@@ -0,0 +1,23 @@
+/**
+ * File Name: DataType.java<br>
+ * Nepton, Jean-francois<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor<br>
+ * Created: Feb 11, 2017
+ */
+package com.sqa.jf.helpers;
+
+/**
+ * DataType //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Nepton, Jean-francois
+ * @version 1.0.0
+ * @since 1.0
+ */
+public enum DataType {
+	BOOLEAN, CHAR, DOUBLE, FLOAT, INT, STRING
+}
---
+++ src/main/java/com/sqa/jf/helpers/TextFormat.java
@@ -0,0 +1,23 @@
+/**
+ * File Name: TextFormat.java<br>
+ * Nepton, Jean-francois<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor<br>
+ * Created: Feb 11, 2017
+ */
+package com.sqa.jf.helpers;
+
+/**
+ * TextFormat //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Nepton, Jean-francois
+ * @version 1.0.0
+ * @since 1.0
+ */
+public enum TextFormat {
+	BFT, CSV, JSON, TAB, XML
+}
